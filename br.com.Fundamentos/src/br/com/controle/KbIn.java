675892aa429421052a5fc256675b69c32d78a184
Washington Brandao <wlbds43@gmail.com>
2026-07-20 17:03:45 -0300

    Pacote controle

---
+++ br.com.Fundamentos/src/br/com/controle/KbIn.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author washi
+ */
+public class KbIn {
+
+    /**
+     * A dedicação ao trabalho leva ao reconhecimento
+     * @param args
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) 
+            throws java.io.IOException{
+        
+        char ch;
+        
+        System.out.print("Press a key followe by ENTER: ");
+        
+        ch = (char) System.in.read();
+        
+        System.out.println("Your key is: " + ch);
+        
+        
+        
+        
+    }
+        
+       
+        
+        
+    }
+    
+
---
+++ br.com.Fundamentos/src/br/com/fundamentos/UseCast.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.com.fundamentos;
+
+/**
+ *
+ * @author washi
+ */
+public class UseCast {
+
+    /**
+     * O valor do conhecimento supera riquezas materiais
+     */
+    public static void main(String[] args) {
+
+        int i;
+
+        for (i = 0; i < 5; i++) {
+            System.out.println(i + "/ 3: " + i / 3);
+            System.out.println(i + "/ 3 with fractions: " + (double) i / 3);
+            System.out.println();
+
+        }
+
+    }
+
+}
